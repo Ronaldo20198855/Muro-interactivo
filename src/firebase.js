b0989bac6733aabfc153ca5a3383cd7cfd45afe4
Ronaldo Lantigua <20198855@itla.edu.do>
2022-08-15 11:04:03 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import 'firebase/firestore'
 
 
   var firebaseConfig = {
-    apiKey: "AIzaSyBJnPHJZWpeZoleE5BY4-CGjCq1e9oXY1o",
-    authDomain: "muro-interactivo-e3cd7.firebaseapp.com",
-    projectId: "muro-interactivo-e3cd7",
-    storageBucket: "muro-interactivo-e3cd7.appspot.com",
-    messagingSenderId: "320218486977",
-    appId: "1:320218486977:web:a3eefb6d82253679249c40"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 const fb = firebase.initializeApp(firebaseConfig);
